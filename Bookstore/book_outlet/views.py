9cc8d3ec228d19d7c4c94673d15e984c1569c881
Aksan07 <aksanpramod1@gmail.com>
2024-12-06 13:46:17 +0530

    first commit

---
+++ Bookstore/book_outlet/views.py
@@ -0,0 +1,18 @@
+from django.shortcuts import render,get_object_or_404
+from .models import Book
+
+# Create your views here.
+def index(request):
+    books=Book.objects.all()
+    return render(request,"book_outlet/index.html",{
+        "books":books,
+    })
+def book_detail(request,id):
+    book = get_object_or_404(Book,pk=id)
+    return render(request,"book_outlet/book_detail.html",{
+        "title":book.title,
+        "rating":book.title,
+        "author":book.author,
+        "is_bestselling":book.is_bestselling,
+    })
+    
